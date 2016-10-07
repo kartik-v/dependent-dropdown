e82042f31cb6346f0a5f423737b7b48136456bb1
Bizley <pawel@positive.codes>
2016-10-07 12:51:00 +0200

    Polish translation

---
+++ js/depdrop_locale_pl.js
@@ -0,0 +1,19 @@
+/*!
+ * Dependent Dropdown Polish Translations
+ *
+ * This file must be loaded after 'dependent-dropdown.js'. Patterns in braces '{}', or
+ * any HTML markup tags in the messages must not be converted or translated.
+ *
+ * @see http://github.com/kartik-v/dependent-dropdown
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+(function ($) {
+    "use strict";
+
+    $.fn.depdropLocales['pl'] = {
+        loadingText: 'Ładowanie ...',
+        placeholder: 'Wybierz ...',
+        emptyMsg: 'Brak danych'
+    };
+})(window.jQuery);
